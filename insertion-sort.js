96ae7b04f3a5c68e7b9621b2b00527855d650cbf
Eva Barrett <Evabarrett23@Gmail.com>
2018-07-31 09:24:56 -0700

    fixed sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,14 +1,14 @@
 let insertionSort = array => {
   for (let i = 1; i < array.length; i++) {
-    let tempIndex = array[i];
-    for (let j = i - 1; j >= 0 && array[j] > tempIndex; j--) {
+    let tempValue = array[i];
+    for (var j = i - 1; j >= 0 && array[j] > tempValue; j--) {
       array[j + 1] = array[j];
     }
-    array[j + 1] = tempIndex;
+    array[j + 1] = tempValue;
   }
   return array;
 }
 
+console.log(insertionSort([2, 1, 22, -5, -1, -5, 50, 2, 13, -3]));
 
-// reference:
-// https://repl.it/@EvaB/JSInsertionSortcode
+// make it an while loop rather than another for loop, local/global variable, changed let to var line 4
