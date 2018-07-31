fa1cfe112fd4a9acf09a9d675c43e59e8bd9d440
Eva Barrett <Evabarrett23@Gmail.com>
2018-07-30 23:48:17 -0700

    insertion js

---
+++ insertion-sort.js
@@ -0,0 +1,10 @@
+let insertionSort = array => {
+  for (let i = 1; i < array.length; i++) {
+    let tempIndex = array[i];
+    for (let j = i - 1; j >= 0 && array[j] > tempIndex; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = tempIndex;
+  }
+  return array;
+}
